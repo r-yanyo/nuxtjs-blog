149b7df809f7556032b5d90c9ee401e9d92d1e69
r-yanyo <honma.ryosuke@gmail.com>
2018-11-28 01:19:52 +0900

    [Add] title & favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,12 @@ import headerItem from "~/components/header-item"
 export default {
   components: {
     headerItem
+  },
+  head() {
+    const title = `r-yanyo.com`;
+    return {
+      title: title,
+    }
   }
 }
 </script>
