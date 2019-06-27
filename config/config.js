d0085328cd79b5353c154c75cd48594a7ed4af9d
yanyo <honma.ryosuke@gmail.com>
2019-06-27 11:34:41 +0900

    コンテンツ管理をcontentfulに移行

---
+++ config/config.js
@@ -0,0 +1,4 @@
+export default {
+  SPACE_ID: 'wcj80e9o9v1k',
+  ACCESS_TOKEN: '8LXrk59PCfMY8rJtBDHaNFQ0IA5UCZnmbqEhwrmMEDU'
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,18 @@
-const pkg = require("./package");
+const pkg = require('./package')
 
-import { sourceFileArray } from "./content/json/summary.json";
+import client from './apis/contentful.js'
 
 const generateDynamicRoutes = callback => {
-  const routes = sourceFileArray.map(sourceFileName => {
-    if (!sourceFileName) return "";
-    sourceFileName = sourceFileName
-      .split("/")
-      .slice(2)
-      .join("/");
-    sourceFileName = sourceFileName.replace(/\.md$/, "");
-    const split = sourceFileName.split("-");
-    const date = split.slice(0, 3).join("-");
-    const slug = split.slice(3).join("-");
-    return `posts/${date}/${slug}`;
-  });
-  callback(null, routes);
-};
+  const routes = client.getEntries().then(contents =>
+    contents.items.map(content => {
+      return `/posts/${content.fields.date}/${content.sys.id}`
+    })
+  )
+  callback(null, routes)
+}
 
 module.exports = {
-  mode: "universal",
+  mode: 'universal',
 
   /*
    ** Headers of the page
@@ -27,34 +20,34 @@ module.exports = {
   head: {
     title: pkg.name,
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: pkg.description },
-      { hid: "og:url", property: "og:url", content: "https://r-yanyo.com/" },
-      { hid: "og:title", property: "og:title", content: "r-yanyoのブログ" },
-      { hid: "og:image", property: "og:image", content: "https://r-yanyo.com/images/ball.png" },
-      { hid: "og:site_name", property: "og:site_name", content: "r-yanyoのブログ" },
-      { hid: "og:type", property: "og:type", content: "blog" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'og:url', property: 'og:url', content: 'https://r-yanyo.com/' },
+      { hid: 'og:title', property: 'og:title', content: 'r-yanyoのブログ' },
+      { hid: 'og:image', property: 'og:image', content: 'https://r-yanyo.com/images/ball.png' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'r-yanyoのブログ' },
+      { hid: 'og:type', property: 'og:type', content: 'blog' },
       {
-        hid: "og:description",
-        property: "og:description",
-        content: "r-yanyoのブログです。"
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'r-yanyoのブログです。'
       },
       // Twitter
-      { name: "twitter:card", content: "summary" }
+      { name: 'twitter:card', content: 'summary' }
     ],
-    link: [{ rel: "icon", href: "/ball.png" }]
+    link: [{ rel: 'icon', href: '/ball.png' }]
   },
 
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: "#fff" },
+  loading: { color: '#fff' },
 
   /*
    ** Global CSS
    */
-  css: ["~/css/main.css"],
+  css: ['~/css/main.css'],
 
   /*
    ** Plugins to load before mounting the App
@@ -79,4 +72,4 @@ module.exports = {
   generate: {
     routes: generateDynamicRoutes
   }
-};
+}
